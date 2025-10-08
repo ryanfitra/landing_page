ed3e1555390cd9721e0be3914c827dccd091d8e5
ryanfitra <ryanfitraperdana@gmail.com>
2025-10-08 12:11:51 +0700

    update route redirect

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingPageController;
 
+Route::get('/', function () {
+    return redirect('/chd');
+});
 Route::get('/chd', [LandingPageController::class, 'index']);
